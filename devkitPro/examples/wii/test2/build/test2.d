33822905b99aa816eb346f8572a9f4906094352c
Alastair Cota <a1.cota.365@gmail.com>
2021-12-01 11:53:44 +0000

    Readded MSYS2

---
+++ devkitPro/examples/wii/test2/build/test2.d
@@ -0,0 +1,114 @@
+test2.o: \
+ C:/Users/Alastair/Documents/GitHub/codemii/devkitPro/examples/wii/test2/source/test2.c \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/gccore.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/dsp.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/gctypes.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/aram.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/gcbool.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/arqueue.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/lwp_queue.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/aram.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/arqmgr.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/audio.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/cache.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/card.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/cast.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/color.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/consol.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gx_struct.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/dvd.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/disc_io.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/exi.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gu.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gx.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/lwp.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gu.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/si.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gx_struct.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/irq.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/context.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/lwp.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/mutex.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/message.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/semaphore.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/pad.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/tpl.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gx.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/system.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/gcutil.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/video.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/video_types.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/usbgecko.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/video_types.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/texconv.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/ipc.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/es.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/stm.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/ios.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/usb.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/isfs.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/conf.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/usbstorage.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/mutex.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/system.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/wiilaunch.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/wiiuse/wpad.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/wiiuse/wiiuse.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/bte/bte.h \
+ D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/bte/bd_addr.h
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/gccore.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/dsp.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/gctypes.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/aram.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/gcbool.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/arqueue.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/lwp_queue.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/aram.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/arqmgr.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/audio.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/cache.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/card.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/cast.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/color.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/consol.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gx_struct.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/dvd.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/disc_io.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/exi.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gu.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gx.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/lwp.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gu.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/si.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gx_struct.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/irq.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/context.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/lwp.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/mutex.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/message.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/semaphore.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/pad.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/tpl.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/gx.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/system.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/gcutil.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/video.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/video_types.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/usbgecko.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/video_types.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/texconv.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/ipc.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/es.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/stm.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/ios.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/usb.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/isfs.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/conf.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/usbstorage.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/mutex.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/system.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/ogc/wiilaunch.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/wiiuse/wpad.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/wiiuse/wiiuse.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/bte/bte.h:
+D:/Games/Dolphin-x64/CodeMii/devkitPro/libogc/include/bte/bd_addr.h:
